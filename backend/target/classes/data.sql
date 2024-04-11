4ecf164c3ba7315a0aa777caae91cae95f7a21a6
Andreas Hadjiantonis <andreashadjiantonis0@gmail.com>
2024-04-11 14:30:48 +0300

    after edit testinig

---
+++ backend/target/classes/data.sql
@@ -0,0 +1,13 @@
+USE rcl;
+
+INSERT INTO users (username, password, total_points) VALUES
+("admin", "adminP@$$word", 0),
+("thanos20", "th@nn", 400),
+("andreas59", "andreW", 500),
+("eleni12", "changeme", 1200),
+("makiskotsabasis", "mpira", 100);
+
+INSERT INTO recycle_items VALUES
+("Glass", 10),
+("Paper", 5),
+("Aluminium", 20);
